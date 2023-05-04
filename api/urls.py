9bf7cf0aaf9d585c40b3bf5a80b567cfc1258fa7
nathan lopez <hiven@hiven.hiven>
2023-05-04 04:07:25 +0200

    site complet -v2.0

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_api, list_article, list_categorie, list_promotion, article_categorie
+from .views import home_api, list_article, list_categorie, list_promotion, article_categorie, mes_informatiion
 
 app_name = "api"
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('list/article', list_article, name="list_article"),
     path('list/categorie', list_categorie, name="list_categorie"),
     path('list/promotion', list_promotion, name="list_promotion"),
+    path('mesinfo', mes_informatiion, name="mes_informations"),
     path('list/categorie/<int:id_categorie>/article', article_categorie, name="article_categorie"),
 ]
